f664e89b2b937966a7e6352725932d6ce873403f
harshal5-dev <harshalganbote55@gmail.com>
2025-06-11 19:11:35 +0530

    Add resilience4j retry and circuit breaker support; enhance build-info endpoint with fallback method

--- accounts/src/main/java/com/trustwave/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/trustwave/accounts/controller/AccountController.java
@@ -6,6 +6,7 @@ import com.trustwave.accounts.dto.CustomerDto;
 import com.trustwave.accounts.dto.ErrorResponseDto;
 import com.trustwave.accounts.dto.ResponseDto;
 import com.trustwave.accounts.service.IAccountsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,6 +15,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -21,6 +24,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeoutException;
 
 @Tag(
         name = "CRUD REST APIs for Accounts in TrustWave",
@@ -37,6 +42,8 @@ public class AccountController {
   private final IAccountsService iAccountsService;
   private final Environment environment;
   private final AccountsContactInfoDto accountsContactInfoDto;
+
+  private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
 
 
   public AccountController(IAccountsService iAccountsService, Environment environment, AccountsContactInfoDto accountsContactInfoDto) {
@@ -194,11 +201,21 @@ public class AccountController {
           )
   }
   )
+  @Retry(name = "getBuildInfo", fallbackMethod = "getBuildInfoFallback")
   @GetMapping("/build-info")
-  public ResponseEntity<String> getBuildInfo() {
+  public ResponseEntity<String> getBuildInfo() throws TimeoutException {
+    logger.debug("getBuildInfo() method called.");
+    // throw new TimeoutException("getBuildInfo() method called.");
     return ResponseEntity
             .status(HttpStatus.OK)
             .body(buildVersion);
+  }
+
+  public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
+    logger.debug("getBuildInfoFallback() method called.");
+    return ResponseEntity
+            .status(HttpStatus.OK)
+            .body("0.9");
   }
 
   @Operation(
