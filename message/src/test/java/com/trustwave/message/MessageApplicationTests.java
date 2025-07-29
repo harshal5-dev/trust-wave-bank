f784f982df007257c5b6ead2be52494755b1737f
harshal5-dev <harshalganbote55@gmail.com>
2025-07-29 18:19:32 +0530

    Add messaging microservice with DTO and function beans; configure Maven and project settings

---
+++ message/src/test/java/com/trustwave/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustwave.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
