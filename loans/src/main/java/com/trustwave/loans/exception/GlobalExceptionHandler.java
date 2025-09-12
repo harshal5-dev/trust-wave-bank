794276c0f7fa76be870b467121ec5323dad792f5
harshal5-dev <harshalganbote55@gmail.com>
2025-09-12 17:01:28 +0530

    Refactor POM files and add Maven wrapper for trustwave-bom; update application.yml for health endpoint configuration

--- loans/src/main/java/com/trustwave/loans/exception/GlobalExceptionHandler.java
+++ loans/src/main/java/com/trustwave/loans/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.trustwave.loans.exception;
 
-import com.trustwave.loans.dto.ErrorResponseDto;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -13,18 +16,15 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.trustwave.loans.dto.ErrorResponseDto;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(
-          MethodArgumentNotValidException ex, @NonNull HttpHeaders headers,@NonNull HttpStatusCode status,@NonNull WebRequest request) {
+      @NonNull MethodArgumentNotValidException ex, @NonNull HttpHeaders headers,
+      @NonNull HttpStatusCode status, @NonNull WebRequest request) {
     Map<String, String> validationErrors = new HashMap<>();
     List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
 
@@ -38,37 +38,25 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
-                                                                WebRequest webRequest) {
-    ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
-            webRequest.getDescription(false),
-            HttpStatus.INTERNAL_SERVER_ERROR,
-            exception.getMessage(),
-            LocalDateTime.now()
-    );
+      WebRequest webRequest) {
+    ErrorResponseDto errorResponseDTO = new ErrorResponseDto(webRequest.getDescription(false),
+        HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage(), LocalDateTime.now());
     return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
   @ExceptionHandler(ResourceNotFoundException.class)
-  public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception,
-                                                                          WebRequest webRequest) {
-    ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
-            webRequest.getDescription(false),
-            HttpStatus.NOT_FOUND,
-            exception.getMessage(),
-            LocalDateTime.now()
-    );
+  public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(
+      ResourceNotFoundException exception, WebRequest webRequest) {
+    ErrorResponseDto errorResponseDTO = new ErrorResponseDto(webRequest.getDescription(false),
+        HttpStatus.NOT_FOUND, exception.getMessage(), LocalDateTime.now());
     return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler(LoanAlreadyExistsException.class)
-  public ResponseEntity<ErrorResponseDto> handleLoanAlreadyExistsException(LoanAlreadyExistsException exception,
-                                                                           WebRequest webRequest){
-    ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
-            webRequest.getDescription(false),
-            HttpStatus.BAD_REQUEST,
-            exception.getMessage(),
-            LocalDateTime.now()
-    );
+  public ResponseEntity<ErrorResponseDto> handleLoanAlreadyExistsException(
+      LoanAlreadyExistsException exception, WebRequest webRequest) {
+    ErrorResponseDto errorResponseDTO = new ErrorResponseDto(webRequest.getDescription(false),
+        HttpStatus.BAD_REQUEST, exception.getMessage(), LocalDateTime.now());
     return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
   }
 }
